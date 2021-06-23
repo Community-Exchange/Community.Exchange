a7c2022c3278620dc34747820fd39bfe7e6705b8
Amber Lee <lee.amber.alex@gmail.com>
2021-06-23 11:07:33 -0700

    Reorganized stack.

---
+++ client/src/components/Forms/ExchangeForm.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./forms.css";
+
+export default function ExchangeForm() {
+  const [need, setNeed] = useState();
+  const [exchange, setExchange] = useState([]);
+  const [description, setDescription] = useState();
+
+  return (
+    <div className="container form-font">
+      <h1 className="form-header">Make an Exchange </h1>
+      <h4 className="form-header">Username: </h4>
+      <Form>
+        <Form.Group>
+          <Form.Label>What do you need?</Form.Label>
+          <Form.Control type="text" placeholder="Help with yard work..." />
+        </Form.Group>
+        <fieldset>
+          <Form.Label>
+            Will you be exchanging an item or services today?
+          </Form.Label>
+          <Form.Group className="check-border">
+            <Form.Check
+              type="checkbox"
+              label="Item"
+              name="Item"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="checkbox"
+              label="services"
+              name="services"
+              id="formHorizontalRadios2"
+            />
+          </Form.Group>
+        </fieldset>
+        ;
+        <Form.Group>
+          <Form.Label>
+            What service or item will you exchange in return?
+          </Form.Label>
+          <Form.Control as="select" multiple>
+            <option>Auto Mechanics</option>
+            <option>Childcare</option>
+            <option>Cleaning</option>
+            <option>Cooking</option>
+            <option>Education/Tutoring</option>
+            <option>Landscaping</option>
+            <option>Pet Care</option>
+            <option>Plumbing</option>
+            <option>Other</option>
+          </Form.Control>
+        </Form.Group>
+        <fieldset>
+          <Form.Label>
+            What service or item will you exchange in return?
+          </Form.Label>
+          <Form.Group className="check-border">
+            {/* <Col sm={12}> */}
+
+            <Form.Check
+              type="checkbox"
+              label="Auto Mechanics"
+              name="Auto Mechanics"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Childcare"
+              name="Childcare"
+              id="formHorizontalRadios2"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Cleaning"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            />
+
+            <Form.Check
+              type="checkbox"
+              label="Cooking"
+              name="Cooking"
+              id="formHorizontalRadios3"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Education/Tutoring"
+              name="Education/Tutoring"
+              id="formHorizontalRadios3"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Landscaping"
+              name="Landscaping"
+              id="formHorizontalRadios3"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Pet Care"
+              name="Pet Care"
+              id="formHorizontalRadios3"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Plumbing"
+              name="Plumbing"
+              id="formHorizontalRadios3"
+            />
+            <Form.Check
+              type="checkbox"
+              label="Other"
+              name="Other"
+              id="formHorizontalRadios3"
+            />
+            {/* </Col> */}
+          </Form.Group>
+        </fieldset>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Additional Information:</Form.Label>
+          <Form.Control as="textarea" rows={10} />
+        </Form.Group>
+        <Form.Group>
+          <Form.File
+            id="exampleFormControlFile1"
+            label="Include an image of your Exchange item."
+          />
+        </Form.Group>
+        <div className="div-button">
+          <Button className="button" variant="outline-secondary">
+            Post Exchange
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import ExchangeForm from "components/Forms/ExchangeForm";
+
+import Nav from "components/Nav/Nav";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Nav />
+      <ExchangeForm />
+    </div>
+  );
+}
