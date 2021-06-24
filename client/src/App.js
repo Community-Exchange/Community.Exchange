6ca5b2c9333b52ec98cbfb18849efef33b581010
Amber Lee <lee.amber.alex@gmail.com>
2021-06-24 16:02:24 -0700

    Merge conflicts.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
-// import Dashboard from "pages/Dashboard";
-import History from "pages/History";
-import "./App.css";
+import Signup from "./components/Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
+import ForgotPass from "./components/ForgotPass";
+import LogoutFunctioning from "./components/LogoutFunctioning";
 
 function App() {
   return (
-    <div >
-      {/* <Dashboard /> */}
-      <History/>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400 px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={LogoutFunctioning} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPass} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
