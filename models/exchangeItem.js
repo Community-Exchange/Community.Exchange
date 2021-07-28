d8687900e7e6c9f71a3fc3291a47754cd0c8c678
Amber Lee <lee.amber.alex@gmail.com>
2021-07-28 15:52:22 -0700

    Added fields to model for exchange.

--- models/exchangeItem.js
+++ models/exchangeItem.js
@@ -1,17 +1,23 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-// const exchangeItemSchema = new Schema({
-//     userID: {
-//         type: String,
-//     },
-//     email: {
-//         type: String,
-//     },
-//     city: {
-//         type: String,
-//     }
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const exchangeItemSchema = new Schema({
+    userID: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+    city: {
+        type: String,
+    },
+    need: {
+        type: String,
+    },
+    details: {
+        type: String,
+    },
 
-// })
+})
 
-// const ExchangeItem = mongoose.model("Exchange", exchangeItemSchema);
-// model.export = ExchangeItem;
+const ExchangeItem = mongoose.model("ExchangeItem", exchangeItemSchema);
+module.exports = ExchangeItem;
